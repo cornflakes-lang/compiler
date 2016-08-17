39bc730ee9e518157e85d9a8d6b2b635d6f93ba6
Alex Bates <16batesa@gmail.com>
2016-08-17 10:00:52 +0100

    fix ast continually growing after parse

--- src/parser.js
+++ src/parser.js
@@ -7,15 +7,15 @@ const path = require('path')
 const grammar = fs.readFileSync(
   path.join(__dirname, '/', 'cornflakes.ne'),
   'utf-8')
-const parser = make(grammar, { require })
 
 module.exports = function parse(string) {
+  const parser = make(grammar, { require })
   const asts = parser.feed(string).results
 
   if(asts.length > 1) {
     console.warn('Ambiguous grammar:')
     console.dir(asts, { depth: null })
   }
-  
+
   return asts[0]
 }
